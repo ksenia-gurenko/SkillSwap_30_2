ebd097543564899a20feb94da2e71dc5eeb1e91a
Kirill22012002 <perepechkina.anastasiya.2002@yandex.ru>
2025-06-08 13:38:13 +0300

    added UI component UserAvatarUI

---
+++ src/shared/ui/user-avatar/user-avatar.tsx
@@ -0,0 +1,16 @@
+import type { FC } from 'react';
+import type { TUserAvatarUIProps } from './type';
+import { USER_AVATAR_SIZE } from '../../lib/constants';
+import styles from './user-avatar.module.css';
+
+export const UserAvatarUI: FC<TUserAvatarUIProps> = ({
+  src,
+  size = USER_AVATAR_SIZE.medium
+}) => {
+  return (
+    <div
+      className={`${styles.avatar} ${styles[size]}`}
+      style={{ backgroundImage: `url(${src})` }}
+    />
+  );
+};
