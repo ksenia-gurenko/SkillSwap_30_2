996b4ae8425195db1d3898155eed2785535224f4
Kirill22012002 <perepechkina.anastasiya.2002@yandex.ru>
2025-06-08 11:32:19 +0300

    added storybook

--- src/shared/ui/stepper/stepper.tsx
+++ src/shared/ui/stepper/stepper.tsx
@@ -2,14 +2,12 @@ import type { FC } from 'react';
 import styles from './stepper.module.css';
 import type { TStepperUIProps } from './type';
 
-export const StepperUI: FC<TStepperUIProps> = ({
-  currentStep,
-  totalSteps
-}) => {
+export const StepperUI: FC<TStepperUIProps> = ({ currentStep, totalSteps }) => {
   return (
     <div className={styles.container}>
       <div className={styles.label}>
-        Шаг {currentStep < totalSteps ? currentStep : totalSteps} из {totalSteps}
+        Шаг {currentStep < totalSteps ? currentStep : totalSteps} из{' '}
+        {totalSteps}
       </div>
       <div className={styles.steps}>
         {Array.from({ length: totalSteps }).map((_, index) => (
