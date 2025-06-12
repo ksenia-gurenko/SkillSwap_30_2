fbe9c79b3be96da007594d4dbdae635480ea3e65
unknown <sabina.guseinowa1995@ya.ru>
2025-06-12 23:11:51 +0300

    сделала папку загаловков, стили, ts, tsx

---
+++ src/shared/ui/section-header/section-header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './section-header.css';
+
+interface SectionHeaderProps {
+  text: string;
+  level?: 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+}
+
+export const SectionHeader: React.FC<SectionHeaderProps> = ({ text, level = 'h2' }) => {
+  switch (level) {
+    case 'h3':
+      return <h3 className="section-header section-header--h3">{text}</h3>;
+    case 'h4':
+      return <h4 className="section-header section-header--h4">{text}</h4>;
+    case 'h5':
+      return <h5 className="section-header section-header--h5">{text}</h5>;
+    case 'h6':
+      return <h6 className="section-header section-header--h6">{text}</h6>;
+    default:
+      return <h2 className="section-header section-header--h2">{text}</h2>;
+  }
+}; 
