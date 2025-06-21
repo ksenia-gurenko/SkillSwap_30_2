7f642a6b22219636785eb6249e62d1f9f9f69c89
Anastasiya1712 <perepechkina.anastasiya.2002@yandex.ru>
2025-06-21 16:59:12 +0300

    fix loginPage

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -14,27 +14,28 @@ export const LoginPage = () => {
             <div className={styles.sections}>
                 <div className={styles.section}>
                     <div className={styles.loginForm}>
-                        <AuthButton icon='google' />
-                        <AuthButton icon='apple' />
+                        <div className={styles.loginProviders}>
+                            <AuthButton icon='google' />
+                            <AuthButton icon='apple' />
+
+                        </div>
                         <div className={styles.separator}>
                             <span className={styles.separatorLine}></span>
                             <span>или</span>
                             <span className={styles.separatorLine}></span>
                         </div>
-                        <div className={styles.inputGroup}>
+                        <div className={styles.inputs}>
                             <Input
                                 label="Email"
                                 placeholder="Введите email"
                                 type='email'
                             />
-                        </div>
-
-                        <div className={styles.inputGroup}>
                             <Input
                                 label="Пароль"
                                 placeholder="Введите ваш пароль"
                                 type='password'
                             />
+
                         </div>
 
                         <Button fill width={436}>Войти</Button>
