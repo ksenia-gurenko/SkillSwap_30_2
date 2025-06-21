23478219ac084fd21314af9d0725809d0ad46763
Anastasiya1712 <perepechkina.anastasiya.2002@yandex.ru>
2025-06-21 15:20:37 +0300

    added Provider to index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/app';
 import { BrowserRouter } from 'react-router-dom';
+import { AppStateProvider } from './entities/app-state-context/app-state-context';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppStateProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppStateProvider>
   </StrictMode>
 );
